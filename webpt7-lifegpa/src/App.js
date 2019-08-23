8706adf2f70fb9e07bc28a435b289ee12070b745
Dereck Lawrence Jr <d.lawrencejr@icloud.com>
2019-08-22 23:09:58 -0400

    Built Signup Form

--- webpt7-lifegpa/src/App.js
+++ webpt7-lifegpa/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DemoComponent from './Components/DemoComponent';
+import FormikSignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <DemoComponent />
+      <FormikSignUp/>
     </div>
   );
 }
